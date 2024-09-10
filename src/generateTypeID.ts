79db1b104ec5c9683c341b534ee76a6c0ade06db
Lukas Runge <login@lukasrunge.de>
2024-09-10 12:51:31 +0200

    💄 run linter

--- src/generateTypeID.ts
+++ src/generateTypeID.ts
@@ -1,5 +1,6 @@
-import { Clipboard, getPreferenceValues, LaunchProps, showHUD, showToast, Toast } from "@raycast/api"
+import { Clipboard, getPreferenceValues, LaunchProps, showHUD, showToast, Toast } from "@raycast/api";
 import { typeid } from "typeid-js";
+
 import { generateUuids } from "./utils/uuidUtils";
 import { UUIDType } from "./uuidHistory";
 
@@ -7,55 +8,56 @@ import { UUIDType } from "./uuidHistory";
 const UUID_MAX_NUMBER = 10000;
 
 export default async function generatetypeid(props: LaunchProps<{ arguments: Arguments.GenerateTypeID }>) {
-    let { prefix, numberOfUUIDsToGenerate } = props.arguments;
-    const { upperCaseLetters, defaultAction } = getPreferenceValues<Preferences.GenerateTypeID>();
+  const { prefix, numberOfUUIDsToGenerate = "1" } = props.arguments;
+  const { upperCaseLetters, defaultAction } = getPreferenceValues<Preferences.GenerateTypeID>();
 
-    if (!numberOfUUIDsToGenerate) {
-      numberOfUUIDsToGenerate = "1";
+  try {
+    const parseableNumber = parseInt(numberOfUUIDsToGenerate, 10);
+
+    if (isNaN(parseableNumber)) {
+      throw new Error("INVALID_NUMBER");
     }
-  
-    try {
-      const parseableNumber = parseInt(numberOfUUIDsToGenerate, 10);
-  
-      if (isNaN(parseableNumber)) {
-        throw new Error("INVALID_NUMBER");
+
+    // safe?
+    if (parseableNumber <= UUID_MAX_NUMBER) {
+      const uuids = await generateUuids(
+        () => typeid(prefix).toString(),
+        parseableNumber,
+        upperCaseLetters,
+        UUIDType.TYPEID
+      );
+
+      if (defaultAction === "copy") {
+        await Clipboard.copy(uuids.join("\r\n"));
+      } else if (defaultAction === "paste") {
+        await Clipboard.paste(uuids.join("\r\n"));
       }
-  
-      // safe?
-      if (parseableNumber <= UUID_MAX_NUMBER) {
-        const uuids = await generateUuids(() => typeid(prefix).toString(), parseableNumber, upperCaseLetters, UUIDType.TYPEID);
-  
-        if (defaultAction === "copy") {
-          await Clipboard.copy(uuids.join("\r\n"));
-        } else if (defaultAction === "paste") {
-          await Clipboard.paste(uuids.join("\r\n"));
-        }
-        const action = defaultAction === "copy" ? "Copied" : "Pasted";
-        const successMessage = uuids.length > 1 ? `${action} ${uuids.length} new UUIDs.` : `${action} new UUID: ${uuids}`;
-        await showHUD(`✅ ${successMessage}`);
-      } else {
+      const action = defaultAction === "copy" ? "Copied" : "Pasted";
+      const successMessage = uuids.length > 1 ? `${action} ${uuids.length} new UUIDs.` : `${action} new UUID: ${uuids}`;
+      await showHUD(`✅ ${successMessage}`);
+    } else {
+      await showToast({
+        style: Toast.Style.Failure,
+        title: "Too many UUIDs requested.",
+        message: `${parseableNumber} exceeds maximum UUIDs of ${UUID_MAX_NUMBER}. Try a lower number.`,
+      });
+    }
+  } catch (e) {
+    switch ((e as { message: string }).message) {
+      case "INVALID_NUMBER":
         await showToast({
           style: Toast.Style.Failure,
-          title: "Too many UUIDs requested.",
-          message: `${parseableNumber} exceeds maximum UUIDs of ${UUID_MAX_NUMBER}. Try a lower number.`,
+          title: "Invalid number.",
+          message: "An invalid number has been provided. Try an actual number.",
         });
-      }
-    } catch (e) {
-      switch ((e as { message: string }).message) {
-        case "INVALID_NUMBER":
-          await showToast({
-            style: Toast.Style.Failure,
-            title: "Invalid number.",
-            message: "An invalid number has been provided. Try an actual number.",
-          });
-          break;
-        case "Invalid prefix. Must be at most 63 ascii letters [a-z_]":
-          await showToast({
-            style: Toast.Style.Failure,
-            title: "Invalid prefix.",
-            message: "Must be at most 63 ascii letters [a-z_]",
-          });
-          break;
-      }
+        break;
+      case "Invalid prefix. Must be at most 63 ascii letters [a-z_]":
+        await showToast({
+          style: Toast.Style.Failure,
+          title: "Invalid prefix.",
+          message: "Must be at most 63 ascii letters [a-z_]",
+        });
+        break;
     }
-};
+  }
+}
